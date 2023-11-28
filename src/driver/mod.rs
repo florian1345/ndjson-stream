cb8e72daa9f835b4b533f667a4bea2be38119db9
florian1345 <florian1345@gmx.de>
2023-11-29 00:13:44 +0100

    expose erroneously private structs

--- src/driver/mod.rs
+++ src/driver/mod.rs
@@ -7,5 +7,11 @@ pub(crate) mod streams;
 #[cfg(feature = "iter")]
 pub use crate::driver::iter::NdjsonIter;
 
+#[cfg(feature = "iter")]
+pub use crate::driver::iter::FallibleNdjsonIter;
+
 #[cfg(feature = "streams")]
 pub use crate::driver::streams::NdjsonStream;
+
+#[cfg(feature = "streams")]
+pub use crate::driver::streams::FallibleNdjsonStream;
