42a0d6dce3ba7b9d1f0a42e1ab6b0a11fae5acf5
florian1345 <florian1345@gmx.de>
2023-11-30 23:07:02 +0100

    rename `streams` module and feature to `stream`

--- src/driver/mod.rs
+++ src/driver/mod.rs
@@ -1,8 +1,8 @@
 #[cfg(feature = "iter")]
 pub(crate) mod iter;
 
-#[cfg(feature = "streams")]
-pub(crate) mod streams;
+#[cfg(feature = "stream")]
+pub(crate) mod stream;
 
 #[cfg(feature = "iter")]
 pub use crate::driver::iter::NdjsonIter;
@@ -10,8 +10,8 @@ pub use crate::driver::iter::NdjsonIter;
 #[cfg(feature = "iter")]
 pub use crate::driver::iter::FallibleNdjsonIter;
 
-#[cfg(feature = "streams")]
-pub use crate::driver::streams::NdjsonStream;
+#[cfg(feature = "stream")]
+pub use crate::driver::stream::NdjsonStream;
 
-#[cfg(feature = "streams")]
-pub use crate::driver::streams::FallibleNdjsonStream;
+#[cfg(feature = "stream")]
+pub use crate::driver::stream::FallibleNdjsonStream;
